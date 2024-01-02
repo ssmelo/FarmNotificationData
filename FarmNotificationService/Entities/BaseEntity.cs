00af681db91eff433323020801a2fea2efee167e
Samuel Melo <samuelmelo00@hotmail.com>
2024-01-02 12:58:15 -0300

    Adicionar arquivos de projeto.

---
+++ FarmNotificationService/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace FarmNotificationService.Entities
+{
+    public class BaseEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+    }
+}
---
+++ FarmNotificationService/Entities/Record.cs
@@ -0,0 +1,15 @@
+﻿namespace FarmNotificationService.Entities
+{
+    public class Record : BaseEntity
+    {
+        public string? Type { get; set; }
+        public DateTime TrackingDate { get; set; }
+        public IEnumerable<TrackingItem> TrackingItems { get; set; } = Enumerable.Empty<TrackingItem>();
+    }
+
+    public class TrackingItem
+    {
+        public DateTime EventTime { get; set; }
+        public string? Value { get; set; }
+    }
+}
